b249849d774fad7f8a4be048bec73bc5520e857c
Shen Baoyong <13261448660@163.com>
2017-04-23 16:25:04 +0800

    集成thymeleaf 测试通过

--- src/main/java/com/shenbaoyong/attendance/controller/TestController.java
+++ src/main/java/com/shenbaoyong/attendance/controller/TestController.java
@@ -1,22 +1,21 @@
 package com.shenbaoyong.attendance.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Shen Baoyong on 2017/4/23.
  */
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/hello")
     public String helloHtml(Model model){
-        model.addAttribute("name", "Dear");
+        model.addAttribute("hello", "你好");
+        model.addAttribute("name", "shenbaoyong");
         return "helloHtml";
     }
 }
